399c768f8db2dfec4c970e65d6ea5bbda1edea06
coffee <nanashongwe@gmail.com>
2023-11-30 04:44:01 +0200

    its 5

--- lesson1.R
+++ lesson1.R
@@ -1,2 +1,21 @@
-#lesson 1
-print("Hello world... looks like python")
+#Data types
+#Vector
+a <- c(1,2,3,4)
+#matrix
+
+data <- runif(28,min = 0, max = 1)
+QData <- c(data >= 0.5)
+cnames <-c("Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Satruday", "Sunday")
+rnames <-c("Week 1","Week 2", "Week 3","Week 4" )
+myMatrix <- matrix(QData, nrow = 4, ncol = 7, byrow=TRUE, dimnames = list(rnames, cnames) )
+
+
+#DataFrame
+#col names
+header <- c("Day of The Week", "Action or Not" )
+dataFrame <- as.data.frame(myMatrix)
+plot(dataFrame$Wednesday)
+dataFrame
+
+#frequency or mode in R
+table(dataFrame$Wednesday)
